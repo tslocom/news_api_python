d42edb4fe81c9f9e7dfeb771cdcfd754d7da2f1d
tslocom <travis.slocom99@gmail.com>
2026-05-14 20:33:54 -0500

    Fixed typo in allowed hosts

--- news_api_python/settings.py
+++ news_api_python/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['news_api_python.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['news_api_python.up.railway.app', 'localhost', '127.0.0.1', '.railway.app']
 
 
 # Application definition
